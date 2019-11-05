9bd71247366c2636f6dcd4c2364579dfecc4fcb8
Jubilian <jubilianharvest@gmail.com>
2019-11-05 22:06:09 +0800

    initial version

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ 'TelegramProductionURL': 'https://api.telegram.org/bot956908533:AAElgoTFOnRJ4MkMmsMZZb6sk_Sz4U9Ojwc/', // do not forget trailing "/" !!
+ 'TelegramToken': '956908533:AAElgoTFOnRJ4MkMmsMZZb6sk_Sz4U9Ojwc', // Telegram token
+ 'googleSheetKey': '2PACX-1vTSB3uppDffbOt02LqrFLfeV-FQu_u64qMgLbZXFfEq4Mg7JB4fz7pkajJNWqJWfxr9Io3IeSV9IvYj', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+}
